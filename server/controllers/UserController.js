86bf3330f89ea446825b991830d3b3dd24518ae3
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-13 11:00:16 -0500

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -8,18 +8,3 @@ export const getAllUsers = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
-
-// export const createUser = async (req, res) => {
-//     const { username, password } = req.body;
-
-//     try {
-//         const result = await pool.query('INSERT INTO "USER" (username, password) VALUES ($1, $2) RETURNING *', [username, password]);
-//         res.status(201).json(result.rows[0]);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
-// Implement these functions if needed
-// export const updateUser = async (req, res) => { ... };
-// export const deleteUser = async (req, res) => { ... };
